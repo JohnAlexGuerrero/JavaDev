09f642698bd57681a778d3ee05204b60e1a64c8b
johnalexguerrero <johnalexguerrero@gmail.com>
2025-01-01 17:13:23 -0500

    modifed project planificador de objetivos

--- PlanificadorDeObjetivosDeAnnoNuevo/src/main/java/org/example/Main.java
+++ PlanificadorDeObjetivosDeAnnoNuevo/src/main/java/org/example/Main.java
@@ -9,73 +9,97 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.HashMap;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // agregar una lista de 10 objetivos para alcanzar en el nuevo año
-        List<Goal> goals = Arrays.asList(
-                new Goal("Leer Libros",24, "Libro", 12),
-                new Goal("Estudiar Git", 1, "curso", 1),
-                new Goal("Hablar Ingles", 1, "Curso", 6),
-                new Goal("Aprender a Tocar Guitarra", 8, "Curso", 8),
-                new Goal("Aprender a Nadar", 2, "Curso", 2),
-                new Goal("ldkfldkf", 45, "Curso", 6)
-        );
+    static List<Goal> goals = new ArrayList<>();
+    static final Scanner scanner = new Scanner(System.in);
+    static HashMap<String, List<Goal>> plan;
+    static String [] months = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+    public static void showMenu(){
+        System.out.println("Planificador de Objetivos");
+        System.out.println("1. Añadir Objetivo");
+        System.out.println("2. Calcular el plan detallado");
+        System.out.println("3. Guardar la planificación");
+        System.out.println("4. Salir");
+    }
 
-        // permitir añadir solo 10 objetivos como maximo
-        if(goals.size() > 10){
-            System.out.println("Solo es permitido maximo 10 Objetivos");
-            System.out.println("Debes eliminar " + (goals.size() - 10));
-            System.exit(1);
-        }
-        System.out.println("Total Objetivos: " + goals.size());
+    public static Goal requestGoal(){
+        System.out.println("Meta: ");
+        String goalName = scanner.nextLine();
+        int amount = 0;
+        int limit;
 
-        // Cada objetivo tiene un plazo maximo de 12 meses
-        boolean anyHaveTimeMaxTwelveMonths = goals.stream()
-                .anyMatch(e -> e.getNumberMonths() > 12);
-
-        if(anyHaveTimeMaxTwelveMonths){
-            System.out.println("existe un objetivo con plazo mayor a 12 meses.");
-            System.out.println("revisa el plazo de tus objetivos.");
-            System.exit(1);
-        }
-
-        // Calcular el plan detallado
-        // arreglo type String con los meses del año
-        String [] months = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
-        // map type key-value donde la key es el plazo en meses para alcanzar el objetivo
-        // el value es una lista de objetivos que tienen la misma duración en meses
-        Map<Integer, List<Goal>> goalsByAmount = goals.stream()
-                .collect(Collectors.groupingBy(Goal::getNumberMonths));
-        // HashMap <String, List<Goal>> para
-        HashMap <String, List<Goal>> plan = new HashMap<>();
-
-        for(String m : months){
-            List <Goal> goalList = new ArrayList<>();
-            for (Integer i: goalsByAmount.keySet()){
-                if(plan.size() >= i){
+        while (amount == 0){
+            System.out.println("Cantidad:");
+            try {
+                amount = Integer.parseInt(scanner.nextLine());
+                if(amount < 0){
+                    System.out.println("La cantidad debe ser un numero positivo");
                     continue;
-                }else{
-                    for(Goal g : goalsByAmount.get(i)){
-                        goalList.add(g);
-                    }
+                }
+                break;
+            }catch (Exception e){
+                System.out.println("Por favor, introduce un número válido");
+            }
+        }
 
+        System.out.println("Unidad: ");
+        String unit = scanner.nextLine();
+
+        while(true){
+            try {
+                System.out.println("Plazo en meses (máximo 12): ");
+                limit = Integer.parseInt(scanner.nextLine());
+
+                if (limit <= 0 || limit > months.length){
+                    System.out.println("El plazo debe ser un número entre 1 y 12 (meses)");
+                    continue;
+                }
+                break;
+
+            }catch (Exception e){
+                System.out.println("Por favor, introduce un número entre 1 y 12 (meses)");
+            }
+        }
+        return new Goal(goalName, amount, unit,limit);
+    }
+
+    public static void calculateDetailedPlan(List<Goal> goals){
+        plan = new HashMap<>();
+
+        Map<Integer, List<Goal>> mapPlan = goals.stream()
+                .collect(Collectors.groupingBy(Goal::getLimit));
+
+        for (int i=0; i < mapPlan.size(); i++){
+            List <Goal> goalList = new ArrayList<>();
+
+            for (Integer j: mapPlan.keySet()){
+                goalList.addAll(mapPlan.get(j));
+                for(int k=0; k < j; k++){
+                    plan.put(months[k], goalList);
                 }
             }
-            plan.put(m, goalList);
         }
+    }
 
+    public static void showDetailedPlan(HashMap<String, List<Goal>> plan){
         //Mostrar la planificación de objetivos por mes
         for (String m: months) {
             System.out.println(m + ":");
             for(Goal g: plan.get(m)){
-                System.out.println(g);
-
+                for (int i=0; i < plan.get(m).size(); i++){
+                    System.out.println(
+                            "[]" + (i+1) + ". " + g.getTitle() + " ("+ g.getUnitByMonth() + " " + g.getUnits() + "/mes). Total: " + g.getAmount() + "."
+                    );
+                }
             }
         }
+    }
 
+    public static void saveDetailedPlan(HashMap<String, List<Goal>> plan){
         // Guardar la planificación en archivo txt
         try {
             File fileData = new File("planificacion.txt");
@@ -84,7 +108,11 @@ public class Main {
                 for (String m: months) {
                     fileWriter.write(m + "\n");
                     for(Goal g: plan.get(m)){
-                        fileWriter.write(g.toString() + "\n");
+                        for (int i=0; i < plan.get(m).size(); i++){
+                            fileWriter.write(
+                                    "[]" + (i+1) + ". " + g.getTitle() + " ("+ g.getUnitByMonth() + " " + g.getUnits() + "/mes). Total: " + g.getAmount() + ".\n"
+                            );
+                        }
                     }
                 }
                 fileWriter.close();
@@ -100,18 +128,74 @@ public class Main {
         }
     }
 
+    public static void main(String[] args) {
+        while (true){
+            showMenu();
+            int option = scanner.nextInt();
+            switch (option){
+                case 1:
+                    // permitir añadir solo 10 objetivos como maximo
+                    if(goals.size() > 10){
+                        System.out.println("Solo es permitido máximo 10 Objetivos");
+                        System.out.println("Debes eliminar " + (goals.size() - 10));
+                    }else{
+                        Goal goal = requestGoal();
+                        goals.add(goal);
+                        System.out.println("Objetivo añadido con éxito.");
+                        System.out.println("Total Objetivos: " + goals.size());
+                    }
+                    break;
+                case 2:
+                    if (goals.size() == 0){
+                        System.out.println("No hay objetivos añadidos.");
+                    }else{
+                        calculateDetailedPlan(goals);
+                        showDetailedPlan(plan);
+                    }
+                    break;
+                case 3:
+                    if (goals.size() == 0){
+                        System.out.println("No hay objetivos añadidos.");
+                    }else{
+                        saveDetailedPlan(plan);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Saliendo del planificador.");
+                    System.exit(1);
+                    break;
+                default:
+                    System.out.println("Opción inválida. Elige una opción entre 1 y 4.");
+
+            }
+        }
+
+/*
+        // Cada objetivo tiene un plazo maximo de 12 meses
+        boolean anyHaveTimeMaxTwelveMonths = goals.stream()
+                .anyMatch(e -> e.getLimit() > 12);
+
+        if(anyHaveTimeMaxTwelveMonths){
+            System.out.println("existe un objetivo con plazo mayor a 12 meses.");
+            System.out.println("revisa el plazo de tus objetivos.");
+            System.exit(1);
+        }
+
+ */
+    }
+
 
     static class Goal{
         private String title;
         private int amount;
         private String units;
-        private int numberMonths;
+        private int limit;
 
-        public Goal(String title, int amount, String units, int numberMonths) {
+        public Goal(String title, int amount, String units, int limit) {
             this.title = title;
             this.amount = amount;
             this.units = units;
-            this.numberMonths = numberMonths;
+            this.limit = limit;
 
         }
 
@@ -127,17 +211,17 @@ public class Main {
             return units;
         }
 
-        public int getNumberMonths() {
-            return numberMonths;
+        public int getLimit() {
+            return limit;
         }
 
         // calcular el numero de unidades aplicadas en cada mes
         public int getUnitByMonth(){
-            if(this.amount == this.numberMonths) return (this.amount / this.numberMonths);
-            if(this.amount < this.numberMonths) return 1;
-            if(this.amount > this.numberMonths) {
-                var resM = Math.round((float) this.amount / this.numberMonths);
-                var resU = this.amount % this.numberMonths;
+            if(this.amount == this.limit) return (this.amount / this.limit);
+            if(this.amount < this.limit) return 1;
+            if(this.amount > this.limit) {
+                var resM = Math.round((float) this.amount / this.limit);
+                var resU = this.amount % this.limit;
                 //System.out.println("r:"+resU);
                 if(resU == 0) return resM;
                 return resM;
@@ -149,7 +233,7 @@ public class Main {
         // cada objetivo debe poseer un nombre, la cantidad de unidades a completar y su total
         @Override
         public String toString() {
-            return "[] 1. " + this.getTitle() + " ("+ this.getUnitByMonth() + " " + this.getUnits() + "/mes). Total: " + this.getAmount() + ".";
+            return "";
         }
     }
 
