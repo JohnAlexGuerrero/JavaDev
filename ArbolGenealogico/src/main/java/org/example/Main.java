27684ed91dcfc60ae0054945dbf870b4d49c2804
johnalexguerrero <johnalexguerrero@gmail.com>
2025-01-08 22:44:35 -0500

    menu

--- ArbolGenealogico/src/main/java/org/example/Main.java
+++ ArbolGenealogico/src/main/java/org/example/Main.java
@@ -5,19 +5,49 @@ import org.example.DAO.PersonDAO;
 import org.example.models.FamilyTree;
 import org.example.models.Person;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        PersonDAO personDAO = new PersonDAO();
-        FamilyDAO familyDAO = new FamilyDAO();
+    static final PersonDAO personDAO = new PersonDAO();
+    static final FamilyDAO familyDAO = new FamilyDAO();
+    static final Scanner scanner = new Scanner(System.in);
+    public static void menu(){
+        System.out.println("2. Familias");
+        System.out.println("1. Agregar Persona.");
+        System.out.println("3. exit.");
+    }
 
-        // create Person
-        //Person p = new Person("Daemon Targaryen", "male");
-        //personDAO.createPerson(p);
+    public static Person personManager(){
+        System.out.println("Nombre: ");
+        String namePerson = scanner.next();
+        System.out.println("Genero (Male / Female): ");
+        String genderPerson = scanner.next();
+        return new Person(namePerson, genderPerson);
+    }
+
+    public static void main(String[] args) {
+        int option;
+
+        while(true){
+            menu();
+            option = scanner.nextInt();
+
+            switch (option){
+                case 1:
+                    Person p = personManager();
+                    personDAO.createPerson(p);
+                    break;
+                case 3:
+                    System.exit(1);
+                default:
+                    System.out.println("Opcion no permitida.");
+            }
+        }
 
         // create family
-        FamilyTree family = new FamilyTree("Targaryen");
-        familyDAO.createFamily(family);
+//        FamilyTree family = new FamilyTree("Targaryen");
+  //      familyDAO.createFamily(family);
     }
 }
